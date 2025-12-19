dc52e08032fa4f0cae5970e19394c253408de778
Mike van der Eerden <github@dotme.nl>
2025-12-19 21:46:24 +0100

    Basic functionality working

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
+use std::process::id;
+
+use rspotify::{
+    model::{PlayableId, PlaylistId, TrackId},
+    prelude::{BaseClient, OAuthClient},
+};
 
 mod onelibrary;
 mod spotify_auth;
 
-// mod spotify_auth;
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
-    //load .env
-    dotenv::dotenv().ok();
-
-    spotify_auth::authenticate_spotify().await;
-    
-    let filepath="example.xml";
+    let spotify = spotify_auth::authenticate_spotify().await;
+    let playlist_name = "DJ Selection";
+    let filepath = "example.xml";
 
     if !std::path::Path::new(filepath).exists() {
         println!("File {} does not exist.", filepath);
@@ -26,6 +26,53 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Ok(_) => println!("Tracks filled successfully."),
         Err(e) => println!("Error filling tracks: {}", e),
     }
+
+    let spotify_user = spotify.current_user().await?;
+
+    let playlist_exists = spotify
+        .user_playlists_manual(spotify_user.id.clone(), Some(50), Some(0))
+        .await
+        .unwrap();
+
+    //println!("Found {} playlists", playlist_exists.total);
+
+    //let mut playlist_id: PlaylistId = PlaylistId::from_id("playlist_id").unwrap();
+    let playlist_id: PlaylistId;
+    let existing_playlist = playlist_exists
+        .items
+        .iter()
+        .find(|p| p.name == playlist_name);
+
+    if let Some(playlist) = existing_playlist {
+        println!("Playlist found: {} - {}", playlist.name, playlist.id);
+        playlist_id = playlist.id.clone();
+        //playlist.tracks;
+    } else {
+        let playlist = spotify
+            .user_playlist_create(
+                spotify_user.id.clone(),
+                playlist_name,
+                Some(false),
+                Some(false),
+                None,
+            )
+            .await?;
+
+        println!("Created playlist: {} - {}", playlist.name, playlist.id);
+
+        playlist_id = playlist.id.clone();
+    }
+
+    //only retain tracks that are not already in the playlist
+
+    
+    let items: Vec<PlayableId> = onelibrary
+        .tracks
+        .iter()
+        .map(|t| PlayableId::from(TrackId::from_id(&t.spotify_id).unwrap()))
+        .collect();
+
+    spotify.playlist_add_items(playlist_id, items.clone(), None).await?;
 
     println!("Total found: {}", onelibrary.tracks.len());
 
--- src/spotify_auth.rs
+++ src/spotify_auth.rs
@@ -1,5 +1,5 @@
 use rspotify::{
-    model::{AdditionalType, Country, Market},
+    //model::{AdditionalType, Country, Market},
     prelude::*,
     scopes, AuthCodeSpotify, Credentials, OAuth,
 };
@@ -10,18 +10,17 @@ pub async fn authenticate_spotify() -> AuthCodeSpotify {
     let scopes = scopes!(
         "playlist-modify-public",
         "playlist-modify-private",
+        "playlist-read-private",
         "user-library-read"
     );
 
     let oauth = OAuth::from_env(scopes).expect("Failed to get Spotify OAuth from environment");
 
-    let mut spotify = AuthCodeSpotify::new(creds, oauth);
-
+    let spotify = AuthCodeSpotify::new(creds, oauth);
 
     // Obtaining the access token
     let url = spotify.get_authorize_url(false).unwrap();
-    println!("Visit this URL to authorize: {}", url);
-    spotify.request_token(&url).await.unwrap();
+    spotify.prompt_for_token(&url).await.unwrap();
 
     spotify
 }
